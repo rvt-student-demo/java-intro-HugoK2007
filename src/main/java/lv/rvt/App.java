eeda7fe5203baa85e7f4efc27b8d154fb9249f7a
HugoK2007 <101929960+HugoK2007@users.noreply.github.com>
2024-09-13 11:26:31 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,27 @@
 package lv.rvt;
 
+import java.util.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your Hello World programm!
+        Scanner scanner = new Scanner(System.in);
+
+     
+        String name = scanner.nextLine();
+
+     
+        String surname = scanner.nextLine();
+
+        
+        String group = scanner.nextLine();
+
+        
+        System.out.println("Students: " + name + " " + surname + "Grupa: " + group );
+
+        
+        // code ends
+        
     }
 }
