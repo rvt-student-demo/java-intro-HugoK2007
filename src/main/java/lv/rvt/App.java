ada97a6900830ddd5e9d908c097481d37889c516
HugoK2007 <101929960+HugoK2007@users.noreply.github.com>
2024-09-27 10:54:32 +0000

    Grade evaluator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,14 +8,32 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give speed: ");
-        int speed = Integer.valueOf(scanner.nextLine());
+        System.out.println("Grade: ");
+        int points = Integer.valueOf(scanner.nextLine());
 
     
-        if (speed > 120) {
-            System.out.println("Speeding Ticket!");
-        } else {
-            System.out.println("Pass.");
+        if (points >= 100) {
+            System.out.println("Perfect");
+        } else if (points >= 90) {
+            System.out.println("5."); 
+
+        } else if (points >= 80) {
+            System.out.println("4.");
+
+        } else if (points >= 70) {
+            System.out.println("3.");
+
+        } else if (points >= 60) {
+            System.out.println("2.");
+
+        } else if (points >= 50) {
+            System.out.println("1.");
+
+        } else if (points <= 49) {
+            System.out.println("failed.");
+
+        } else if (points <= 0) {
+            System.out.println("IMPOSSIBLE");
         }
         // code ends
         
