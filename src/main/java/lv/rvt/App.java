d083d8b8168bf0a3f0ba23ff807cb6508981ee66
HugoK2007 <101929960+HugoK2007@users.noreply.github.com>
2024-09-27 11:29:23 +0000

    Gift Tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,34 +8,26 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Grade: ");
-        int points = Integer.valueOf(scanner.nextLine());
+        System.out.println("Value of the gift?");
 
-    
-        if (points >= 100) {
-            System.out.println("Perfect");
-        } else if (points >= 90) {
-            System.out.println("5."); 
+        int val = Integer.valueOf(scanner.nextLine());
 
-        } else if (points >= 80) {
-            System.out.println("4.");
-
-        } else if (points >= 70) {
-            System.out.println("3.");
-
-        } else if (points >= 60) {
-            System.out.println("2.");
-
-        } else if (points >= 50) {
-            System.out.println("1.");
-
-        } else if (points <= 49) {
-            System.out.println("failed.");
-
-        } else if (points <= 0) {
-            System.out.println("IMPOSSIBLE");
+        if(val < 5000){
+            System.out.println("No tax!");
+        }else if(val >= 5000 && val < 25000){
+            System.out.println("Tax: "+((double)100+(val-5000)*0.08));
+        }else if(val >= 25000 && val < 55000){
+            System.out.println("Tax: "+((double)1700+(val-25000)*0.1));
+        }else if(val >= 55000 && val < 200000){
+            System.out.println("Tax: "+((double)4700+(val-55000)*0.12));
+        }else if(val >= 200000 && val < 1000000){
+            System.out.println("Tax: "+((double)22100+(val-200000)*0.15));
+        }else{
+            System.out.println("Tax: "+((double)142100+(val-1000000)*0.17));
         }
-        // code ends
-        
     }
 }
+        // code ends
+        
+    
+
