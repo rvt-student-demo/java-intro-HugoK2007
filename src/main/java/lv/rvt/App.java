49808f0e4ac32200d091728ae567cebd32c8a44b
HugoK2007 <101929960+HugoK2007@users.noreply.github.com>
2024-10-10 12:02:34 +0000

    count to 100

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,30 +4,18 @@ import java.util.*;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        Scanner scanner = new Scanner(System.in);
 
-        double sum = 0;
-        double amt = 0;
-
-        while (true) {
-            System.out.println("Give a number: ");
-            double numb = Double.valueOf(scanner.nextLine());
-            if (numb == 0) {
-                System.out.println("Number of numbers: " + amt);
-                System.out.println("Sum of the numbers: " + sum);
-                break;
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+    
+            for (int i = input; i <= 100; i++) {
+                System.out.println(i);
             }
-            else{
-                sum = sum + numb;
-                amt = amt + 1;
-            }
-            
-
         }
     }
-}
+    
+
         // code ends
         
     
