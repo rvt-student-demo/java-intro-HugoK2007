48bdbe7a886e76d6041f23289561ce695825f36e
HugoK2007 <101929960+HugoK2007@users.noreply.github.com>
2024-10-17 10:59:20 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,19 +8,21 @@ public class App
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             
-          
-            System.out.println("How many times?");
-            int amount = scanner.nextInt();
-    
-            for (int i = 0; i < amount; i++) {
-                printText();
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+
+        divisibleByThreeInRange(n1, n2);
+
+    }
+
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
             }
         }
-    
-        public static void printText() {
-            System.out.println("In a hole in the ground there lived a method");
-        }
     }
+}
         
     
     
