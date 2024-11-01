1053fdff4d40a4b07702f6717387f47b53fdfab8
HugoK2007 <101929960+HugoK2007@users.noreply.github.com>
2024-11-01 12:27:25 +0000

    PersonalDetails

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,26 +4,29 @@ import java.util.*;
 
 public class App 
 {
-
-  public static void main ( String[] args )
-  {
-    int[] val = {0, 1, 2, 3}; 
-    int temp;
- 
-    System.out.println( "Original Array: " 
-        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
- 
-    // reverse the order of the numbers in the array
-    for(int i = 0; i < val.length / 2; i++) {
-      // Swapping the elements
-      int j = val[i];
-      val[i] = val[val.length - i - 1];
-      val[val.length - i - 1] = j;
-  }
- 
-    
- 
-    System.out.println( "Reversed Array: " 
-        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
-   }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int count = 0;
+        double average = 0.0;
+        int sum = 0;
+        int longest = 0;
+        String longestName = "";
+        
+        while(!input.isEmpty()){
+            String[] data = input.split(",");
+            
+            if(data[0].length() > longest){
+                longest = data[0].length();
+                longestName = data[0];
+            }
+            sum += Integer.valueOf(data[1]);
+            count++;
+            input = scanner.nextLine();
+        }
+        average = 1.0 * sum/count;
+        System.out.println("Longest name:" + longestName);
+        System.out.println("Average of the birth years:" + average);
+        
+    }
 }
